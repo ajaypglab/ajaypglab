9d4474320b1eedd70cb53f4cd5ad1de2828dbf49
ajaypglab <ajaypglab@gmail.com>
2021-06-24 19:02:11 -0400

    with header

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
+const welcome = require('cli-welcome');
 
-const clearConsole = require('clear-any-console');
-
-//clears the console
-clearConsole();
+welcome({
+	title: `Welcome CLI`,
+	tagLine: `by Ajay PG`,
+	bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: true,
+	version: `v1.0`
+});
 
 console.log(`Welcome, Ajay PG Lab.
 This is my first CLI project!`);
