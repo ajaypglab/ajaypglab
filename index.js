31a44ef255f3e21691a689a45c1533d9b687c03c
ajaypglab <ajaypglab@gmail.com>
2021-06-24 18:50:38 -0400

    patch

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const clearConsole = require('clear-any-console');
 //clears the console
 clearConsole();
 
-console.log(`Welcome, Ajay PG Lab!
+console.log(`Welcome, Ajay PG Lab.
 This is my first CLI project!`);
