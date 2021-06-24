a97ac8440ac936837e6aadc2f9decdc2852c27da
ajaypglab <ajaypglab@gmail.com>
2021-06-24 19:07:04 -0400

    welcome header

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
+const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
 
 welcome({
 	title: `Welcome CLI`,
 	tagLine: `by Ajay PG`,
+    description: pkgJSON.description,    
 	bgColor: `#FADC00`,
 	color: `#000000`,
 	bold: true,
 	clear: true,
-	version: `v1.0`
+	version: pkgJSON.version
 });
 
 console.log(`Welcome, Ajay PG Lab.
